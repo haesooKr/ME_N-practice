11a71403b068eee5aef3b8404f00870eb54e3a52
haesooKr <gotn676@gmail.com>
2020-03-02 22:45:24 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view-engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000);
