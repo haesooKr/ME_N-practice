1d967c088b8e15603beb920bb9bff71a15f68ce7
haesooKr <gotn676@gmail.com>
2020-03-03 23:04:43 -0500

    Add search authors

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All Authors Route
-router.get('/', (req, res) => {
-  res.render('authors/index')
+router.get('/', async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name != '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', { 
+      authors: authors, 
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
 });
 
 // New Author Route
